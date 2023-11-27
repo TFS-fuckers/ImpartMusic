6d7f5cb008d9442fcd17b666adcff8c74f459684
StarDuster142857 <2637632054@qq.com>
2023-11-27 11:05:59 +0800

    UI

---
+++ impartmusic/src/test/java/MusicTvController.java
@@ -0,0 +1,44 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+
+public class MusicTvController {
+
+    @FXML
+    private ImageView imgViewIcon;
+
+    @FXML
+    private TextArea jfxTextAreaIntro;
+
+    @FXML
+    private Label labTitle;
+
+    @FXML
+    private BorderPane music_pane;
+
+    @FXML
+    private TableColumn<?, ?> tColumn_add_to_queue;
+
+    @FXML
+    private TableColumn<?, ?> tColumn_first;
+
+    @FXML
+    private TableColumn<?, ?> tColumn_music;
+
+    @FXML
+    private TableColumn<?, ?> tColumn_operate;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    void buildnewInputPane(ActionEvent event) {
+
+    }
+
+}
