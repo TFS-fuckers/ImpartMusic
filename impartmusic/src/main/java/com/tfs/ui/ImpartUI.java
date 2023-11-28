64247f5589dcfd23cf90870ba7f49a23197d875f
Ferpakenameyea <65767282+Ferpakenameyea@users.noreply.github.com>
2023-11-28 16:18:58 +0800

    更新客户端代码，预备连接UI

--- impartmusic/src/main/java/com/tfs/ui/ImpartUI.java
+++ impartmusic/src/main/java/com/tfs/ui/ImpartUI.java
@@ -1,4 +1,7 @@
 package com.tfs.ui;
+import com.tfs.client.Client;
+import com.tfs.logger.Logger;
+
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -6,6 +9,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
 public class ImpartUI extends Application {
+    public static boolean isAlive = false;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("music_table.fxml"));
@@ -19,15 +24,22 @@ public class ImpartUI extends Application {
     public void init() throws Exception{
         super.init();
         System.out.println("init()...");
+        isAlive = true;
     }
 
     @Override
     public void stop() throws Exception{
         super.stop();
+        Client.INSTANCE().saveMusicList();
         System.out.println("stop()...");
+        isAlive = false;
     }
 
     public static void showUI() {
-        new Thread(() -> Application.launch(ImpartUI.class)).start();
+        new Thread(() -> {
+            Thread.currentThread().setName("UIThread");
+            Logger.logInfo("Client UI started");
+            Application.launch(ImpartUI.class);
+        }).start();
     }
 }
