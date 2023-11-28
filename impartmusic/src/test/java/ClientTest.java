6d2e8783b8a464c21d384af9b38197239fec68f0
StarDuster142857 <2637632054@qq.com>
2023-11-28 16:30:44 +0800

    UI

--- impartmusic/src/test/java/ClientTest.java
+++ impartmusic/src/test/java/ClientTest.java
@@ -1,7 +1,8 @@
 import com.tfs.client.Client;
 
 public class ClientTest {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         new Client();
     }
 }
