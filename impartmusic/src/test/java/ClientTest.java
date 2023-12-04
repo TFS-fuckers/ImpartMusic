51c4a2a6946bccf452010e08214cdc0ac54ffa1c
StarDuster142857 <2637632054@qq.com>
2023-12-04 10:09:33 +0800

    UI

--- impartmusic/src/test/java/ClientTest.java
+++ impartmusic/src/test/java/ClientTest.java
@@ -1,7 +1,8 @@
 import javafx.embed.swing.JFXPanel;
 
 import com.tfs.client.Client;
-
+import com.tfs.musicplayer.MusicPlayer;
+import java.io.File;
 public class ClientTest {
     public static void main(String[] args) {
         new JFXPanel();
